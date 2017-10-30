f88a6b03823ef6c5a9705c4f950b443f972c4b72
Harmless <harmless@syneforge.com>
2017-10-30 16:32:31 +0200

    Init

--- Application/Module/Subnet/module.php
+++ Application/Module/Subnet/module.php
@@ -19,7 +19,16 @@ require_once 'Model/model.php';
  */
 class Subnet extends Module
 {
-
+    /**
+     * Subnet constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->addRoute('delete');
+        $this->addRoute('create');
+        $this->addRoute('save');
+    }
 }
 
 spl_autoload_register(
