ab96f280d81de20f86c308ab2059eb3a5f57bcd9
Harmless <harmless@syneforge.com>
2017-10-30 15:45:39 +0200

    Init

--- application/config/config.inc.php
+++ application/config/config.inc.php
@@ -1,28 +1,24 @@
 <?php
 
 /**
-* ProAUT Project Configuration file
+* DHCP WEB Project Configuration file
 **/
 
 //namespace Core;
 
 $config = array(
- 'PROJECT_NAME' => 'ProFTPd Administrate Users Tool',
+ 'PROJECT_NAME' => 'DHCP Administrate Tool',
  'PROJECT_VERSION' => '0.0.1',
  'DEFAULT_THEME' => 'Default',
  'CLASS_PATH' => 'application/classes',
  'CONTROLLERS_PATH' => 'application/controllers',
  'MODELS_PATH' => 'application/models',
  'VIEWS_PATH' => 'application/views',
- 'DEFAULT_UID' => 65534,
- 'DEFAULT_GID' => 65534,
- 'DEFAULT_SHELL' => '/bin/nonexistent',
- 'QUOTA' => true,
  'mysql' => array(
   'host' => 'localhost',
-  'database' => 'proftp',
-  'user' => 'proftp',
-  'password' => 'proftp',
+  'database' => 'dhcp',
+  'user' => 'dhcp',
+  'password' => 'dhcp',
   'charset' => 'utf8',
   ),
 );
