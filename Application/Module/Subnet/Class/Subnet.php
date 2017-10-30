4b388e9625975878e6586f97000349fa52e78ebb
Harmless <harmless@syneforge.com>
2017-10-30 17:13:07 +0200

    Init

--- Application/Module/Subnet/Class/Subnet.php
+++ Application/Module/Subnet/Class/Subnet.php
@@ -2,25 +2,62 @@
 
 namespace Module\Subnet;
 
+use Core\Module\Database\DatabaseObject;
+
 /**
  * Class Subnet
  * @package Module\Subnet
  */
-class Subnet extends \Core\Module\Database\DatabaseObject
+class Subnet extends DatabaseObject
 {
     /**
-     * Example constructor.
+     * @var string
      */
-    public function __construct()
-    {
-
-    }
+    protected static $table_name = "subnets";
+    /**
+     * @var array
+     */
+    protected static $db_fields = array('subnet_id', 'dhcp_lease_time', 'dhcp_renewal', 'dhcp_rebind_time', 'mask', 'gateway', 'dns1', 'dns2', 'domain', 'vlan_id', 'type');
+    /**
+     * @var
+     */
+    protected $subnet_id;
+    /**
+     * @var
+     */
+    protected $dhcp_lease_time;
+    /**
+     * @var
+     */
+    protected $dhcp_renewal;
+    /**
+     * @var
+     */
+    protected $dhcp_rebind_time;
+    /**
+     * @var
+     */
+    public $dns1;
+    /**
+     * @var
+     */
+    public $dns2;
+    public $domain;
+    public $gateway;
+    public $mask;
+    public $vlan_id;
+    public $type;
 
     /**
-     * @return bool|string
+     * @param array $item
+     * @return static
      */
-    public function display()
+    public static function add(array $item)
     {
-        return file_get_contents(__DIR__ . "/../View/main.php");
+        $new = new static;
+        foreach ($item as $key => $value) {
+            $new->$key = trim($value);
+        }
+        return $new;
     }
 }
