4aa8dd2e76c2a32a4ec8c2c5f030442ee927fceb
harmless <harmless@syneforge.com>
2017-11-01 01:58:29 +0200

    fix ajax

--- Application/Module/Route/Model/model.php
+++ Application/Module/Route/Model/model.php
@@ -36,6 +36,7 @@ class Model extends \Core\Model
     public function get()
     {
         $query = func_get_arg(0)->getQuery();
+        print_r($query);
         if (isset($query['id'])) {
             $data = Route::find_by_id(intval($query['id']));
             $template = file_get_contents(__DIR__ . '/../View/route_show.php');
