485dfe6ad8b84a02b9b5a88eeee9b0d4f192e0cd
Harmless <harmless@syneforge.com>
2017-11-01 11:28:15 +0200

    Reset project

--- Application/Config/config.php
+++ Application/Config/config.php
@@ -8,4 +8,24 @@
 
 print_r(__FILE__);
 
-return [];
+return ['PROJECT_NAME' => 'DHCP web',
+    'PROJECT_VERSION' => '0.0.1',
+    'PROJECT_ROOT' => dirname(__DIR__, 2),
+    'DEFAULT_THEME' => 'Example',
+    'CLASS_PATH' => 'Application/classes',
+    'INCLUDES_PATH' => 'Application/includes',
+    'MODULE_PATH' => 'Application/Module',
+    'CORE_PATH' => 'Application/Core',
+    'CORE_MODULE_PATH' => 'Application/Core/Module',
+    'THEME_PATH' => 'Application/Theme',
+
+    'THEME' => 'Example',
+
+    'mysql' => array(
+        'host' => 'localhost',
+        'database' => 'dhcpd',
+        'user' => 'dhcpd',
+        'password' => 'dhcpd',
+        'charset' => 'utf8',
+    ),
+];
