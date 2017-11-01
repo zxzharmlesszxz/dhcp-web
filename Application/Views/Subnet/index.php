9e44f70d770d400724f21cb5539ee9282f048ace
Harmless <harmless@syneforge.com>
2017-11-01 18:07:54 +0200

    Added routes

---
+++ Application/Views/Subnet/index.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harmless
+ * Date: 01.11.17
+ * Time: 16:44
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <title>%title%</title>
+</head>
+<body>
+<header id="header">
+    <div id="logo">
+        <a href="/">%title%</a>
+    </div>
+    <nav id="menu">
+        <menu>
+            %menu%
+        </menu>
+        <br class="clearfix">
+    </nav>
+</header>
+<div id="wrapper">
+    <div id="page">
+        <div id="content">
+            <div class="box">
+                %content%
+            </div>
+            <br class="clearfix">
+        </div>
+        <br class="clearfix">
+    </div>
+    <div id="page-bottom">
+        <div id="page-bottom-sidebar">
+            <h3>Contacts</h3>
+            <ul class="list">
+                <li class="last">email: zxzharmlesszxz@gmail.com</li>
+            </ul>
+        </div>
+        <div id="page-bottom-content">
+            <h3>About</h3>
+            <p>
+                Empty yet
+            </p>
+        </div>
+        <br class="clearfix">
+    </div>
+</div>
+<footer id="footer">
+    <a href="https://github.com/zxzharmlesszxz/"></a>
+    - %copyright% &copy; 2015-%date%
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
