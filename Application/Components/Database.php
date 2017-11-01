d699487f1f66f2989759b77eaff99024f52c23dc
Harmless <harmless@syneforge.com>
2017-11-01 16:15:40 +0200

    Added routes

--- Application/Components/Database.php
+++ Application/Components/Database.php
@@ -12,7 +12,7 @@ class Database
     {
         $params = include_once __DIR__ . "/../Config/db_params.php";
         $type = array_keys($params)[0];
-        print_r($type);
+        print_r($params);
         $db = new PDO("$type:host=$params[$type]['host'];dbname=$params[$type]['dababase'];charset=$params[$type]['charset']",
             $params[$type]['user'], $params[$type]['password']);
         return $db;
