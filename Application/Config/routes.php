18f276935dbad1e724e1e25fdc8a50935712f973
Harmless <harmless@syneforge.com>
2017-11-01 13:56:29 +0200

    Added routes

---
+++ Application/Config/routes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harmless
+ * Date: 01.11.17
+ * Time: 13:51
+ */
+
+return [
+    'subnets' => 'subnet/index',
+    'routes' => 'route/index'
+];
--- Application/Router/Router.php
+++ Application/Router/Router.php
@@ -13,10 +13,12 @@ class Router
     public function __construct()
     {
         echo __METHOD__ . "\n";
+        $this->routes = include_once __DIR__ . "/../Config/routes.php";
     }
 
     public function run()
     {
         echo __METHOD__ . "\n";
+        print_r($this->routes);
     }
 }
